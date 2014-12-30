691fd19f9993d0ff5dc2964bde6a355a97021660
Brennan <brecon@microsoft.com>
2014-12-30 13:50:41 -0800

    Initial Redis Message Bus Ported from 2.2.0 Using new DI and newer version of StackExchange.Redis There is sometimes an issue with reconnect but should be fixed in a later version of StackExchange.Redis

---
+++ src/Microsoft.AspNet.SignalR.Redis/RedisScaleoutConfiguration.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Globalization;
+using Microsoft.AspNet.SignalR.Messaging;
+
+namespace Microsoft.AspNet.SignalR.Redis
+{
+    public class RedisScaleoutConfiguration : ScaleoutConfiguration
+    {
+        public RedisScaleoutConfiguration()
+            : this("localhost", 6379, "", "")
+        {
+        }
+
+        public RedisScaleoutConfiguration(string server, int port, string password, string eventKey)
+            : this(CreateConnectionString(server, port, password), eventKey)
+        {
+        }
+
+        public RedisScaleoutConfiguration(string connectionString, string eventKey)
+        {
+            if(connectionString == null)
+            {
+                throw new ArgumentNullException("connectionString");
+            }
+
+            if(eventKey == null)
+            {
+                throw new ArgumentNullException("eventKey");
+            }
+
+            ConnectionString = connectionString;
+            EventKey = eventKey;
+        }
+
+        public string ConnectionString { get; set; }
+
+        public int Database { get; set; }
+
+        public string EventKey { get; set; }
+
+        public static string CreateConnectionString(string server, int port, string password)
+        {
+            return string.Format(CultureInfo.CurrentCulture, "{0}:{1}, password={2}, abortConnect=false", server, port, password);
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.SignalR.Redis/RedisServiceCollectionExtensions.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.AspNet.SignalR.Redis;
+using Microsoft.AspNet.SignalR.Messaging;
+
+namespace Microsoft.Framework.DependencyInjection
+{
+    public static class RedisServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSignalRRedis(this IServiceCollection services, Action<RedisScaleoutConfiguration> configureOptions = null)
+        {
+            return services.AddSignalRRedis(configuration: null, configureOptions: configureOptions);
+        }
+
+        public static IServiceCollection AddSignalRRedis(this IServiceCollection services, IConfiguration configuration, Action<RedisScaleoutConfiguration> configureOptions)
+        {
+            services.AddSingleton<IMessageBus, RedisMessageBus>();
+            services.AddSingleton<IRedisConnection, RedisConnection>();
+
+            if (configuration != null)
+            {
+                services.Configure<RedisScaleoutConfiguration>(configuration);
+            }
+
+            if(configureOptions != null)
+            {
+                services.Configure(configureOptions);
+            }
+
+            return services;
+        }
+
+        //public static IServiceCollection AddSignalRRedis(this IServiceCollection services, string server, int port, string password, string eventKey)
+        //{
+        //    var configuration = new RedisScaleoutConfiguration(server, port, password, eventKey);
+
+        //    return AddSignalRRedis(services, configuration);
+        //}
+
+        //public static IServiceCollection AddSignalRRedis(this IServiceCollection services, RedisScaleoutConfiguration configuration)
+        //{
+        //    //var describer = new ServiceDescriber(configuration);
+        //    //services.TryAdd(describer.Transient<IConfigureOptions<RedisOptions>, RedisOptionsSetup>());
+        //    //services.TryAdd(describer.Singleton<IMessageBus, RedisMessageBus>());
+            
+        //    services.AddTransient<IConfigureOptions<RedisScaleoutConfiguration>, RedisOptionsSetup>();
+        //    services.AddSingleton<IMessageBus, RedisMessageBus>();
+
+        //    //if (configuration != null)
+        //    {
+        //        services.Configure<RedisScaleoutConfiguration>(configuration);
+        //    }
+
+        //    return services;
+        //}
+    }
+}
