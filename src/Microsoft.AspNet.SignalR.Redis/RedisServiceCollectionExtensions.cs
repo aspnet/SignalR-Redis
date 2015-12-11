73cd4a17b52c3a692d3f4d1633a5f12aae140f5d
Pranav K <prkrishn@hotmail.com>
2015-12-10 16:41:07 -0800

    Reacting to Options changes

--- src/Microsoft.AspNet.SignalR.Redis/RedisServiceCollectionExtensions.cs
+++ src/Microsoft.AspNet.SignalR.Redis/RedisServiceCollectionExtensions.cs
@@ -2,9 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNet.SignalR.Redis;
 using Microsoft.AspNet.SignalR.Messaging;
+using Microsoft.AspNet.SignalR.Redis;
+using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
